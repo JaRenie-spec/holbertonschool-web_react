43b8b667aa6724103e2424e7b51b96ac2472ecfc
JaRenie-spec <renie.jeanalain@gmail.com>
2025-05-27 15:22:50 +0200

    task 2

---
+++ task_2/js/main.ts
@@ -0,0 +1,31 @@
+interface Teacher {
+  firstName: string;
+  lastName: string;
+  fullTimeEmployee: boolean;
+  yearsOfExperience?: number;
+  location: string;
+  [propName: string]: any;
+}
+
+interface Directors extends Teacher {
+  numberOfReports: number;
+}
+
+const teacher3: Teacher = {
+  firstName: 'John',
+  lastName: 'Doe',
+  fullTimeEmployee: true,
+  location: 'New York',
+  contract: false,
+};
+
+const director1: Directors = {
+  firstName: 'John',
+  lastName: 'Doe',
+  location: 'London',
+  fullTimeEmployee: true,
+  numberOfReports: 17,
+};
+
+console.log(teacher3);
+console.log(director1);
