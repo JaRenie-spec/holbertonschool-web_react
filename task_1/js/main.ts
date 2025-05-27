2211e2ecbb4b1cf646230ffe1889d4076f310136
JaRenie-spec <renie.jeanalain@gmail.com>
2025-05-27 15:13:38 +0200

    add task 1

---
+++ task_1/js/main.ts
@@ -0,0 +1,18 @@
+interface Teacher {
+  firstName: string;
+  lastName: string;
+  fullTimeEmployee: boolean;
+  yearsOfExperience?: number;
+  location: string;
+  [propName: string]: any;
+}
+
+const teacher3: Teacher = {
+  firstName: 'John',
+  lastName: 'Doe',
+  fullTimeEmployee: true,
+  location: 'New York',
+  contract: false,
+};
+
+console.log(teacher3);
